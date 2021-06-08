1bf505d58c2a4b1ad340713e3b194ab21ab89819
RaviTejaKaranam <ravitejakaranam1999@gmail.com>
2021-06-08 08:16:08 +0530

    update DisplayFiles

--- src/DisplayFiles/DisplayFiles.js
+++ src/DisplayFiles/DisplayFiles.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import "./DisplayFiles.css";
 const DisplayFiles = () => {
   const { files } = JSON.parse(localStorage.getItem("user"));
@@ -7,24 +8,31 @@ const DisplayFiles = () => {
     return <p>Please add some files</p>;
   } else {
     display = files.map((file) => {
-      if(!file.file){
-        console.log(file.error.code)
+      if (file.error.code !== "LIMIT_FILE_SIZE") {
+        console.log(file.error.code);
+        return (
+          <div key={file.file} className="file-box">
+            <li className="file-list">
+              <a href={file.location} key={file.file}>
+                <i className="fas fa-file fa-4x file-icon"></i>
+              </a>
+              <p>{`${file.file.slice(0, 5)}...${file.file.slice(
+                file.file.length - 3
+              )}`}</p>
+            </li>
+          </div>
+        );
+      } else {
+        toast.error("File limit exceeded");
       }
-      return (
-        <div key={file.file} className="file-box">
-          <li className="file-list">
-            <a href={file.location} key={file.file}>
-              <i className="fas fa-file fa-4x file-icon"></i>
-            </a>
-            <p>{`${file.file.slice(0, 5)}...${file.file.slice(
-              file.file.length - 3
-            )}`}</p>
-          </li>
-        </div>
-      );
     });
   }
-  return <div className="display-files">{display}</div>;
+  return(
+  <Fragment>
+    <ToastContainer />
+    <div className="display-files">{display}</div>
+  </Fragment>
+  )
 };
 
 export default DisplayFiles;
